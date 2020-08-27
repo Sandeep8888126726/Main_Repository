6a5e94243ff5adaa343a341910e397f6c7dbfb80
Sandeep8888126726 <70278083+Sandeep8888126726@users.noreply.github.com>
2020-08-27 21:18:42 +0530

    Change no 4

--- src/class_test.java
+++ src/class_test.java
@@ -4,7 +4,7 @@ public class class_test {
 	public void print_message()
 	{
 		System.out.println("Print under Test Class");
-		System.out.println("Print statement for change no 3");
+		System.out.println("Print statement for change no 4");
 	}
 	
 }
