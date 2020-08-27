c0393e319e73e7507c7d10911a99f7fa77821c86
User <User@192.168.0.2>
2020-08-27 21:11:45 +0530

    Chnage no 2

--- src/Main_class.java
+++ src/Main_class.java
@@ -3,7 +3,7 @@ public class Main_class {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Print in main class");
+		System.out.println("Print in main class + code update");
 		class_test cls_test = new class_test();
 		cls_test.print_message();
 	}
