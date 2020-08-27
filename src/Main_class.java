d30659f623204884fc9e97f60953f0f8b6016b61
User <User@192.168.0.2>
2020-08-27 21:07:12 +0530

    Sandeep Chnage no 1

---
+++ src/Main_class.java
@@ -0,0 +1,11 @@
+
+public class Main_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Print in main class");
+		class_test cls_test = new class_test();
+		cls_test.print_message();
+	}
+
+}
---
+++ src/class_test.java
@@ -0,0 +1,9 @@
+
+public class class_test {
+
+	public void print_message()
+	{
+		System.out.println("Print under Test Class");
+	}
+	
+}
